80616cb3baa1203434486ef7220aecd0251ac1ce
siddhantprateek <siddhantprateek@gmail.com>
2024-12-16 16:49:22 +0530

    updated packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,79 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 import tailwindAnimate from "tailwindcss-animate";
-import daisyui from "daisyui";
+// import daisyui from "daisyui";
 
-module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {},
-  		animation: {
-  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-  			'background-position-spin': 'background-position-spin 3000ms infinite alternate',
-  			meteor: 'meteor 5s linear infinite',
-  			marquee: 'marquee var(--duration) infinite linear',
-  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
-  			'shiny-text': 'shiny-text 8s infinite'
-  		},
-  		keyframes: {
-  			'border-beam': {
-  				'100%': {
-  					'offset-distance': '100%'
-  				}
-  			},
-  			'background-position-spin': {
-  				'0%': {
-  					backgroundPosition: 'top center'
-  				},
-  				'100%': {
-  					backgroundPosition: 'bottom center'
-  				}
-  			},
-  			meteor: {
-  				'0%': {
-  					transform: 'rotate(215deg) translateX(0)',
-  					opacity: '1'
-  				},
-  				'70%': {
-  					opacity: '1'
-  				},
-  				'100%': {
-  					transform: 'rotate(215deg) translateX(-500px)',
-  					opacity: '0'
-  				}
-  			},
-  			marquee: {
-  				from: {
-  					transform: 'translateX(0)'
-  				},
-  				to: {
-  					transform: 'translateX(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'marquee-vertical': {
-  				from: {
-  					transform: 'translateY(0)'
-  				},
-  				to: {
-  					transform: 'translateY(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'shiny-text': {
-  				'0%, 90%, 100%': {
-  					'background-position': 'calc(-100% - var(--shiny-width)) 0'
-  				},
-  				'30%, 60%': {
-  					'background-position': 'calc(100% + var(--shiny-width)) 0'
-  				}
-  			}
-  		}
-  	}
-  },
-  plugins: [tailwindAnimate, daisyui],
+export default {
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+theme: {
+	extend: {
+		borderRadius: {
+			lg: 'var(--radius)',
+			md: 'calc(var(--radius) - 2px)',
+			sm: 'calc(var(--radius) - 4px)'
+		},
+		colors: {}
+	}
+},
+plugins: [tailwindAnimate],
 }
